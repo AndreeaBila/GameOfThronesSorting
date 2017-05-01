8eb31608702b11fc0dbc03c84b46fe6faae3f038
AndreeaBila <andreeabila@yahoo.com>
2017-05-01 22:08:48 +0300

    footer.php

---
+++ php/footer.php
@@ -0,0 +1,16 @@
+<footer class="container">
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
+
+    <div class="footerRight">
+        <a href="#" target="_blank"><img src="../img/facebook.png" alt="Facebook"></a>
+        <a href="#" target="_blank"><img src="../img/twitter.png" alt="Twitter"></a>
+        <a href="#" target="_blank"><img src="../img/wordpress.png" alt="WordPress"></a>
+    </div>
+
+    <div class="footerLeft">
+        <p>Copyright Andreea Bîlă & Andreea Diaconu</p>
+    </div>
+
+    <div class="clear"></div>
+
+</footer>
