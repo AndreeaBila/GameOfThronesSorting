c475382bc266f3ae476c3b3133db3c2ed81bd85e
SpecDI <vlad@herghelegiu.com>
2017-05-01 14:07:51 +0300

    Created database classes and database connection

---
+++ php/createAccount.php
@@ -0,0 +1,8 @@
+<?php
+    //file needed to create a user account on the database
+
+    //create database connection
+    $db = new mysqli('localhost', 'root', '', 'westermoredb') or die("Couldn't realize database connection");
+
+    //retrieve the data from the front end application
+?>
