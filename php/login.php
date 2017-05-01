a634034c8c63b84c8783b8710306c0725ac21fe2
AndreeaBila <andreeabila@yahoo.com>
2017-05-01 23:11:07 +0300

    login

---
+++ php/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en-Us">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Westermore</title>
+
+        <!-- Bootstrap -->
+        <!--<link href="../Bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+        <!-- FontAwesome -->
+        <link rel="stylesheet" href="../../FontAwesome/css/font-awesome.min.css">
+        <!-- My CSS -->
+        <link href="../css/login.css" rel="stylesheet">
+        <link rel="shortcut icon" href="../img/westermore2.ico">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="../js/login.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        
+        
+        <?php include 'footer.php'; ?>
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!--<script src="../Bootstrap/js/bootstrap.min.js"></script>-->
+    </body>
+</html>
