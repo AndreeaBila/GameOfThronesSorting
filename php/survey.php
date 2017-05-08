2244fe1d68c7089769c7574b7545c29af035547f
AndreeaBila <andreeabila@yahoo.com>
2017-05-08 20:42:49 +0300

    survey.php & survey.css done

--- php/survey.php
+++ php/survey.php
@@ -5,18 +5,18 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>Westermore</title>
+        <title>Title</title>
 
         <!-- Bootstrap -->
-        <!--<link href="../Bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+        <link href="../Tools/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <!-- FontAwesome -->
         <!--<link rel="stylesheet" href="../../FontAwesome/css/font-awesome.min.css">-->
         <!-- My CSS -->
-        <link href="../css/survey.css" rel="stylesheet">
-        <link rel="shortcut icon" href="../img/westermore2.ico">
+        <link href="style.css" rel="stylesheet">
+        <!--<link rel="shortcut icon" href="../img/.ico">-->
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="../js/survey.js"></script>
+        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="../js/actions.js"></script>-->
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -26,31 +26,35 @@
         <![endif]-->
     </head>
     <body>
-       
-        <div class="answearRight">
-            <form>
-                <input type="radio" name="answear" id="firstAnswear"><span>Stark</span><br>
-                <input type="radio" name="answear" id="secondAnswear"><span>Lannister</span><br>
-                <input type="radio" name="answear" id="thirdAnswear"><span>Targaryen</span><br>
+        
+        <section class="pull-right col-md-5 answear-section">
+            <form class="answear-form">
+                <div class="radio">
+                    <label><input type="radio" name="option" id="option1">Stark</label>
+                </div>
+                <div class="radio">
+                    <label><input type="radio" name="option" id="option2">Lannister</label>
+                </div>
+                <div class="radio">
+                    <label><input type="radio" name="option" id="option3">Targaryen</label>
+                </div>
             </form>
-            
-            <input type="button" name="submitSurveyAnswearButton" id="submitSurveyAnswearButton" value="Submit">
-        </div>
 
-        <section class="questionLeft">
-            <br>
-            <div class="questionDiv">
-                <p class="questionText" id="surveyQuestion">Which is your favorite house</p>
-            </div>   
-            <<br>
+            <button class="btn btn-lg pull-right my-btn" type="submit">Submit</button>
         </section>
 
-        <div class="clear"></div>
-    
-    <!--jQuery (necessary for Bootstrap's JavaScript plugins) 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+        <section class="pull-left col-md-7 text-center fill question-section">
+            <div class="question-div">
+                <p class="question-p" id="question">Which is your favorite class?</p>
+            </div>
+        </section>
+
+
+        
+       <!--jQuery (necessary for Bootstrap's JavaScript plugins)--> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <!--<script src="../Bootstrap/js/bootstrap.min.js"></script>-->
+    <script src="../Tools/Bootstrap/js/bootstrap.min.js"></script>
     <!-- The js script for this file -->
     </body>
 </html>
