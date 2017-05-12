6712da02da431f9e5015f2b3075a4e4a25b4ea4b
SpecDI <vlad@herghelegiu.com>
2017-05-12 23:11:26 +0300

    size

--- php/createHouseConnection.php
+++ php/createHouseConnection.php
@@ -3,9 +3,13 @@
     $db = new mysqli('localhost', 'root', '', 'westermoredb') or die("Error");
     $houseName = strip_tags($_GET['name']);
     $userID = session_id();
-    //get the id of the house with the given name
     $query = "UPDATE Users SET HouseID = (SELECT HouseID FROM Houses WHERE(Name = ?)) WHERE (UserID = $userID);";
     $stmt = $db->prepare($query);
     $stmt->bind_param("s", $houseName);
     $stmt->execute();
+    $query = "UPDATE Houses SET Size = Size + 1 WHERE(Name = ?)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("s", $houseName);
+    $stmt->execute();
+
 ?>
