50b405a05e28dbeb369b9702ba575f68eb0549d9
AndreeaBila <andreeabila@yahoo.com>
2017-05-12 04:01:17 +0300

    sortingSucces.php & css

---
+++ php/sortingSuccess.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en-Us">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>You have been sorted!</title>
+
+        <!-- Bootstrap -->
+        <link href="../../Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- FontAwesome -->
+        <link rel="stylesheet" href="../../FontAwesome/css/font-awesome.min.css">
+        <!-- My CSS -->
+        <link href="../css/sortingSuccess.css" rel="stylesheet">
+        <link rel="shortcut icon" href="../img/westermore2.ico">
+
+        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="../js/actions.js"></script>-->
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        
+        <div class="container">
+            <div class="jumbotron">
+                <h1 id="addHouseResult">You're a Lannister</h1>
+                <h2>Congrats! You have been sorted into your rightful Westerosi home</h2>
+                <p id="addPresentation">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor labore quidem sint porro facilis quisquam odit deserunt molestiae eaque, temporibus aut soluta fugiat rem nemo, culpa distinctio repellendus placeat. Neque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque laudantium amet quo, deserunt mollitia, culpa ipsam, pariatur quis quasi reiciendis, iste enim nulla. Non, facilis quo, illo similique molestias sequi?  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam laborum veritatis ad dolores quos maxime perferendis similique quasi deserunt, eveniet atque, impedit itaque assumenda quae ratione. Incidunt omnis impedit inventore?</p>
+                <p><a class="btn btn-primary btn-lg my-btn" href="#" role="button">Learn more</a></p>
+            </div>
+        </div>
+
+        
+       <!--jQuery (necessary for Bootstrap's JavaScript plugins)--> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="../../Bootstrap/js/bootstrap.min.js"></script>
+        <!-- The js script for this file -->
+        <script src="../js/survey.js"></script>
+    </body>
+</html>
