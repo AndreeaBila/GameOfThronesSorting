c50dd8ce35a42d59d34948d8b2a93a035d39a6db
SpecDI <vlad@herghelegiu.com>
2017-07-20 20:10:14 +0300

    center element works

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
     //get the house id from the database
-    $.getJSON('../php/getHouseID', function(data) {
+    $.getJSON('../php/getHouseID.php', function(data) {
         var houseID = data;
         if (houseID == 1) {
             window.location.href = '../php/welcome';
--- js/margin.js
+++ js/margin.js
@@ -1,24 +1,33 @@
 $(function() {
 
-  //Function that places the element on the center of the page
- 
-    var marginTop = ($(window).height() -  $('.to-page-center').height()) / 2;
-    marginTop = marginTop + 'px';
+    //Function that places the element on the center of the page
 
-    var marginLeft = ($(window).width() -  $('.to-page-center').width()) / 2;
-    marginLeft = marginLeft + 'px';
+    // var marginTop = ($(window).height() - $('.to-page-center').height()) / 2;
+    // var marginLeft = ($(window).width() - $('.to-page-center').width()) / 2;
 
-    console.log('document');
-    console.log($(window).height());
-    console.log('jumbotron');
-    console.log($(this).height());
-    console.log('marginTop');
-    console.log(marginTop);
-    console.log('marginLeft');
-    console.log(marginLeft);
+    // console.log('window height: ' + $(window).height());
+    // console.log('window width: ' + $(window).width());
+    // console.log('jumbotron height: ' + $('.to-page-center').height());
+    // console.log('jumbotron width: ' + $('.to-page-center').width());
+    // console.log('marginTop: ' + marginTop);
+    // console.log('marginLeft: ' + marginLeft);
 
-     $('.to-page-center').css({'margin-top': marginTop, 'margin-left': marginLeft, 'margin-bottom': '0px'});
- 
-    
-  
+    // $('.to-page-center').css({ 'margin-top': marginTop, 'margin-left': marginLeft, 'margin-bottom': marginTop, 'margin-right': marginLeft });
+
+
+
+    jQuery.fn.center = function(parent) {
+        if (parent) {
+            parent = this.parent();
+        } else {
+            parent = window;
+        }
+        this.css({
+            "position": "absolute",
+            "top": ((($(parent).height() - this.outerHeight()) / 2) + $(parent).scrollTop() + "px"),
+            "left": ((($(parent).width() - this.outerWidth()) / 2) + $(parent).scrollLeft() + "px")
+        });
+        return this;
+    }
+    $(".to-page-center").center(false);
 });
