fe5a2f73817cde197bb1543bcf9b307d31e30973
SpecDI <vlad@herghelegiu.com>
2017-07-24 21:49:03 +0300

    secure layer added for getPosts.php

--- php/getPosts.php
+++ php/getPosts.php
@@ -4,14 +4,17 @@
     require 'createConnection.php';
     //import the user file to have access to the user class
     require_once "Post.php";
-    $limit = $_GET['number'];
-    $query = "SELECT * FROM posts WHERE(PostID != 1) ORDER BY DateCreated DESC, PostID DESC LIMIT $limit;";
-    $response = $db->query($query);
+    $limit = strip_tags($_GET['number']);
     $array = array();
-    while($row = mysqli_fetch_array($response, MYSQLI_ASSOC)){
-        $post = new Post($row['PostID'], $row['Title'], $row['Content'], $row['DateCreated'], $row['UserID'], $row['HouseID']);
+    $query = "SELECT * FROM posts WHERE(PostID != 1) ORDER BY DateCreated DESC, PostID DESC LIMIT ?;";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s', $limit);
+    $stmt->execute();
+    $stmt->bind_result($postID, $title, $content, $dateCreated, $userID, $houseID);
+    while($stmt->fetch()){
+        $post = new Post($postID, $title, $content, $dateCreated, $userID, $houseID);
         array_push($array, $post);
     }
-
+    $stmt->close();
     echo json_encode($array);
 ?>
