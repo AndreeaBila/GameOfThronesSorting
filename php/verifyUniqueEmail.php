6ba6dce4a2b142fe440ce728eadb9c80e6e32919
SpecDI <vlad@herghelegiu.com>
2017-08-15 23:04:44 +0300

    create account bug fixed

--- php/verifyUniqueEmail.php
+++ php/verifyUniqueEmail.php
@@ -11,14 +11,14 @@
   //method needed to check if the given email by the user is unique
     function checkEmail($db, $email){
         $count = 0;
-      //retrieve every email address from the database
-      $response = $db->query("SELECT Email FROM users;");
-      //loop thorugh the email addresses and check if there are duplciates
-      while($row = mysqli_fetch_array($response, MYSQLI_ASSOC)){
-          if($row['Email'] == $email){
-              $count++;
-          }
-      }
-      echo $count;
+        //retrieve every email address from the database
+        $response = $db->query("SELECT Email FROM users;");
+        //loop thorugh the email addresses and check if there are duplciates
+        while($row = mysqli_fetch_array($response, MYSQLI_ASSOC)){
+            if($row['Email'] == $email){
+                $count++;
+            }
+        }
+        echo $count;
     }
 ?>
